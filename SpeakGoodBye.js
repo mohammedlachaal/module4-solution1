0d5849d0f592bb3ad7db48497c710b059a1f66d8
lachaal mohammed <med.lachaal.2017@gmail.com>
2020-06-14 20:45:20 +0000

    new commit

---
+++ SpeakGoodBye.js
@@ -0,0 +1,4 @@
+var byeSpeaker = "Good Bye";
+function speak(names) {
+  console.log(byeSpeaker + " " + name);
+}
---
+++ SpeakHello.js
@@ -0,0 +1,4 @@
+var helloSpeaker = "Hello";
+function speak(names) {
+  console.log(helloSpeaker + " " + name);
+}
